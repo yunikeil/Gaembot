a4efbb39aa071456daca3f106c92a6b74f939964
iyunakov <76445770+yunikeil@users.noreply.github.com>
2023-02-16 20:42:24 +0300

    change gameCir.. and .gitignore files

--- gameCirulli.py
+++ gameCirulli.py
@@ -1,20 +1,31 @@
 
 
-class GameCirulli():
+class GameCirulli:
     def __init__(self, size: int):
         """
         На вход, при создании объекта передаётся размер квадрата
         Также тут объявляются все переменные которые будут использоваться
          в последующих методах.
         """
+        self.data: list[list[int]] = [
+            [0 for x in range(size)] for y in range(size)
+        ]
+        print(self.data)
         ...
 
     def __del__(self):
         ...
 
+    def drow_matrix(self, ):
+        """
+        Рисует картинку по двумерному массиву
+        """
+
+        return image
+
     def generate_points(self, ):
         """
-        Генерация рандомный точек в матрице
+        Генерация рандомных точек в матрице
         """
 
         ...
@@ -26,33 +37,29 @@ class GameCirulli():
         Движеное вправо
         """
 
-        ...
+        return self.drow_matrix()
 
     def move_left(self, ):
         """
         Движение влево
         """
 
-        ...
+        return self.drow_matrix()
 
     def move_up(self, ):
         """
         Движеное вверх
         """
 
-        ...
+        return self.drow_matrix()
 
     def move_down(self, ):
         """
         Движение вниз
         """
 
-        ...
-
-    def drow_matrix(self, ):
-        """
-        Рисует картинку по двумерному массиву в данных
-        """
-
-        ...
+        return self.drow_matrix()
     ...
+
+
+abc = GameCirulli(4)
