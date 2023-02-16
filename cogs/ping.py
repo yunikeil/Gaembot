ac7c57202cd856fed19c9d542e2f62dfe5b35060
iyunakov <76445770+yunikeil@users.noreply.github.com>
2023-02-16 14:04:07 +0300

    -

---
+++ cogs/ping.py
@@ -0,0 +1,16 @@
+from discord.ext.commands import Bot, Cog, slash_command
+
+
+class Ping(Cog):
+    def __init__(self, bot: Bot) -> None:
+        self.bot = bot
+
+    def __del__(self):
+        ...
+
+    @slash_command(name="ping", description="A simple ping command.")
+    async def ping(self, inter) -> None:
+        await inter.respond(f"Pong! {self.bot.latency * 1000:.2f}ms")
+
+def setup(bot: Bot) -> None:
+    bot.add_cog(Ping(bot))
