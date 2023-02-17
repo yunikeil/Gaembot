e0326998f5e18285bbeeb7b2ed44f244cfbed987
Ilia Yunakov <76445770+yunikeil@users.noreply.github.com>
2023-02-17 20:55:10 +0300

    -

--- docs/examples/app_commands/slash_cog_groups.py
+++ docs/examples/app_commands/slash_cog_groups.py
@@ -1,23 +1,46 @@
 # This example requires the 'members' privileged intent to use the Member converter.
 
 import discord
-from discord import SlashCommandGroup
+from discord.commands import SlashCommandGroup
 from discord.ext import commands
 
 intents = discord.Intents.default()
 intents.members = True
 
-bot = discord.Bot(debug_guilds=[1075733298371899433],
-                  intents=intents, owner_id=286914074422280194)
+bot = discord.Bot(debug_guilds=[...], intents=intents, owner_id=...)  # Main file
 
 
 class Example(commands.Cog):
     def __init__(self, bot_: discord.Bot):
         self.bot = bot_
 
-    greetings = SlashCommandGroup(name="greetings",
-                                  description="Various greeting from cogs!",
-                                  guild_ids=[1075733298371899433])
+    greetings = SlashCommandGroup("greetings", "Various greeting from cogs!")
+
+    international_greetings = greetings.create_subgroup(
+        "international", "International greetings"
+    )
+
+    secret_greetings = SlashCommandGroup(
+        "secret_greetings",
+        "Secret greetings",
+        checks=[
+            commands.is_owner().predicate
+        ],  # Ensures the owner_id user can access this group, and no one else
+    )
+
+    @greetings.command()
+    async def hello(self, ctx: discord.ApplicationContext):
+        await ctx.respond("Hello, this is a slash subcommand from a cog!")
+
+    @international_greetings.command()
+    async def aloha(self, ctx: discord.ApplicationContext):
+        await ctx.respond("Aloha, a Hawaiian greeting")
+
+    @secret_greetings.command()
+    async def secret_handshake(
+        self, ctx: discord.ApplicationContext, member: discord.Member
+    ):
+        await ctx.respond(f"{member.mention} secret handshakes you")
 
     @commands.Cog.listener()
     async def on_application_command_error(
@@ -26,7 +49,7 @@ class Example(commands.Cog):
         if isinstance(error, commands.NotOwner):
             await ctx.respond("You can't use that command!")
         else:
-            await ctx.respond(f"```\n{error}\n```")
+            raise error  # Raise other errors so they aren't ignored
 
 
 bot.add_cog(Example(bot))  # Put in a setup function for cog files
