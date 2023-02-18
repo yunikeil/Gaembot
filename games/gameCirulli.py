05266fc2416a585bd38d751607122621900eeb6c
Artem Abakumov <113025408+Artem00000000@users.noreply.github.com>
2023-02-18 22:03:07 +0300

    -

--- games/gameCirulli.py
+++ games/gameCirulli.py
@@ -1,3 +1,4 @@
+from random import randint
 
 
 class GameCirulli:
@@ -12,6 +13,20 @@ class GameCirulli:
         self.data: list[list[int]] = [
             [0 for x in range(size)] for y in range(size)
         ]
+        number = randint(1, 100)
+        position1 = randint(1, size ** 2)
+        position2 = randint(1, size ** 2)
+        while position1 == position2:
+            position2 = randint(1, size ** 2)
+        if number < 79:
+            self.data[(position1 - 1) // size][position1 % 4 - 1] = 2
+            self.data[(position2 - 1) // size][position2 % 4 - 1] = 2
+        elif number == 80:
+            self.data[(position1 - 1) // size][position1 % 4 - 1] = 4
+            self.data[(position2 - 1) // size][position2 % 4 - 1] = 4
+        else:
+            self.data[(position1 - 1) // size][position1 % 4 - 1] = 2
+            self.data[(position2 - 1) // size][position2 % 4 - 1] = 4
         ...
 
     def __del__(self):
@@ -54,8 +69,45 @@ class GameCirulli:
         Возвращает фолс если генерация невозможна, тру если всё ок, меняет self.data)
         """
         # Тут код
-        
-        return 
+        countZeros = 0
+        for i in range(self.size):
+            for j in range(self.size):
+                if self.data[i][j] == 0:
+                    countZeros += 1
+        ZeroNumber = randint(1, countZeros)
+        number = 0
+        if randint(1, 8) == 8:
+            newNumeric = 4
+        else:
+            newNumeric = 2
+        for i in range(self.size):
+            for j in range(self.size):
+                if self.data[i][j] == 0:
+                    number += 1
+                    if number == ZeroNumber:
+                        self.data[i][j] = newNumeric
+                        break
+            if number == ZeroNumber:
+                break
+        if countZeros > 1:
+            return True
+        result = False
+        for i in range(1, self.size-1):
+            for j in range(1, self.size-1):
+                if self.data[i][j] == self.data[i][j-1] or self.data[i][j] == self.data[i-1][j] or self.data[i][j] == self.data[i][j+1] or self.data[i][j] == self.data[i+1][j]:
+                    result = True
+                    break
+            if result:
+                break
+        if not result:
+            for i in range(1, self.size-1):
+                if self.data[0][i] == self.data[0][i-1] or self.data[0][i] == self.data[0][i+1] or self.data[self.size-1][i] == self.data[self.size-1][i-1] or self.data[self.size-1][i] == self.data[self.size-1][i+1]:
+                    result = True
+                    break
+                if self.data[i][0] == self.data[i-1][0] or self.data[i][0] == self.data[i+1][0] or self.data[i][self.size-1] == self.data[i-1][self.size-1] or self.data[i][self.size-1] == self.data[i+1][self.size-1]:
+                    result = True
+                    break
+        return result
         ...
 
     def move_right(self):
@@ -65,10 +117,31 @@ class GameCirulli:
         Нет входных значений, кроме самого объекта (self)
         Возвращает результат выполнения drow_matrix
         """
-
-        if self.generate_points():
+        done = False
+        i = 0
+        while i < self.size:
+            j = self.size-2
+            afterUnion = False
+            while j > -1:
+                if j != self.size - 1  and self.data[i][j] != 0:
+                    if self.data[i][j+1] == 0:
+                        done = True
+                        self.data[i][j+1] = self.data[i][j]
+                        self.data[i][j] = 0
+                        j += 2
+                    elif self.data[i][j+1] == self.data[i][j] and not afterUnion:
+                        done = True
+                        self.data[i][j+1] *= 2
+                        self.data[i][j] = 0
+                        afterUnion = True
+                    else:
+                        afterUnion = False
+                j -= 1
+            i += 1
+        if not done or self.generate_points():
             return self.drow_matrix()
-        else: return False
+        else:
+            return False
 
     def move_left(self):
         """
@@ -77,10 +150,31 @@ class GameCirulli:
         Нет входных значений, кроме самого объекта (self)
         Возвращает результат выполнения drow_matrix
         """
-
-        if self.generate_points():
+        done = False
+        i = 0
+        while i < self.size:
+            j = 1
+            afterUnion = False
+            while j < self.size:
+                if j != 0 and self.data[i][j] != 0:
+                    if self.data[i][j-1] == 0:
+                        done = True
+                        self.data[i][j-1] = self.data[i][j]
+                        self.data[i][j] = 0
+                        j -= 2
+                    elif self.data[i][j-1] == self.data[i][j] and not afterUnion:
+                        done = True
+                        self.data[i][j-1] *= 2
+                        self.data[i][j] = 0
+                        afterUnion = True
+                    else:
+                        afterUnion = False
+                j += 1
+            i += 1
+        if not done or self.generate_points():
             return self.drow_matrix()
-        else: return False
+        else:
+            return False
 
     def move_up(self):
         """
@@ -89,10 +183,31 @@ class GameCirulli:
         Нет входных значений, кроме самого объекта (self)
         Возвращает результат выполнения drow_matrix
         """
-
-        if self.generate_points():
+        done = False
+        i = 0
+        while i < self.size:
+            j = 1
+            afterUnion = False
+            while j < self.size:
+                if j != 0 and self.data[j][i] != 0:
+                    if self.data[j-1][i] == 0:
+                        done = True
+                        self.data[j-1][i] = self.data[j][i]
+                        self.data[j][i] = 0
+                        j -= 2
+                    elif self.data[j-1][i] == self.data[j][i] and not afterUnion:
+                        done = True
+                        self.data[j-1][i] *= 2
+                        self.data[j][i] = 0
+                        afterUnion = True
+                    else:
+                        afterUnion = False
+                j += 1
+            i += 1
+        if not done or self.generate_points():
             return self.drow_matrix()
-        else: return False
+        else:
+            return False
 
     def move_down(self):
         """
@@ -101,8 +216,29 @@ class GameCirulli:
         Нет входных значений, кроме самого объекта (self)
         Возвращает результат выполнения drow_matrix
         """
-
-        if self.generate_points():
+        done = False
+        i = 0
+        while i < self.size:
+            j = self.size - 2
+            afterUnion = False
+            while j > -1:
+                if j != self.size - 1 and self.data[j][i] != 0:
+                    if self.data[j+1][i] == 0:
+                        done = True
+                        self.data[j+1][i] = self.data[j][i]
+                        self.data[j][i] = 0
+                        j += 2
+                    elif self.data[j+1][i] == self.data[j][i] and not afterUnion:
+                        done = True
+                        self.data[j+1][i] *= 2
+                        self.data[j][i] = 0
+                        afterUnion = True
+                    else:
+                        afterUnion = False
+                j -= 1
+            i += 1
+        if not done or self.generate_points():
             return self.drow_matrix()
-        else: return False
+        else:
+            return False
     ...
