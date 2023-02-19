c9cb542c01c03040a0c0f8620e369d9252b387cd
Ilia Yunakov <76445770+yunikeil@users.noreply.github.com>
2023-02-19 19:27:21 +0300

    -

--- .test.py
+++
@@ -1,8 +0,0 @@
-from gameCirulli import GameCirulli
-
-
-game = GameCirulli(4)
-#game.data = [[2, 0, 2, 4], [2, 0, 4, 0], [0, 4, 2, 8], [0, 2, 0, 0]]
-print(game.data)
-print(game.generate_points())
-print(game.data)
