1b33d3ddb844edfbcd28fb95ea8b6b0902db8cad
iyunakov <iyunakov@notemail.ru>
2023-04-06 14:16:59 +0300

    after deleted code!

--- cogs/gamesCog.py
+++ cogs/gamesCog.py
@@ -31,7 +31,11 @@ class GamesCog(Cog):
         game_name: str = SlashOption(
             name="game",
             description="The game you want",
-            choices=["2048", "Tic Tac Toe", "Checkers"]
+            choices=[
+                "2048",
+                "Tic Tac Toe",
+                "Checkers"
+            ]
         ),
     ):
 
@@ -39,32 +43,15 @@ class GamesCog(Cog):
         Тут будет отправка сообщений о настройках игры и подключение новых кнопок (от классов игры)
         """
         if game_name == "2048":
-            game = GameCirulli(4)
-            data = '\n'.join('\t'.join(map(str, row)) for row in game.data)
-            e = nextcord.Embed()
-            e.set_image(url=)
-            #byte_im = buf.getvalue()
-            """
-            Во первых можно изменить всё 
-            СОздаётся сообщение с настройкой после кидает на новый канал игры определённого пользователя
-            и там работа идёт с сообщениями а не командами приложений.
-            Также есть вариант временного сохранения картинки и чтения его после
-            """
-            await interaction.response.send_message(f"2048\n{data}", view=GameCirulliView(game))
+            await interaction.response.send_message(
+                f"2048"
+            )
         elif game_name == "Tic Tac Toe":
-            await interaction.response.send_message(f"Tic Tac Toe: X goes first", view=TicTacToe())
-        else:
-            """
-            @nextcord.ui.button(label="0", style=nextcord.ButtonStyle.red)
-            async def count(self, button: nextcord.ui.Button, interaction: nextcord.Interaction):
-                number = int(button.label) if button.label else 0
-                if number >= 10:
-                    button.style = nextcord.ButtonStyle.green
-                    button.disabled = True
-                button.label = str(number + 1)
-                await interaction.response.edit_message(view=self)
-            """
-            await interaction.response.send_message(f"This is {game_name}!")
+            await interaction.response.send_message(
+                f"Tic Tac Toe: X goes first", view=TicTacToe()
+            )
+        elif game_name == "Checkers":
+            await interaction.response.send_message(f"This is Checkers!")
 
 
 def setup(bot: Bot) -> None:
