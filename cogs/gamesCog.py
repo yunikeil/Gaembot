b3fd697d6bebd9c0bcf192dc8583614c6bb9da3f
iyunakov <iyunakov@notemail.ru>
2023-04-06 14:21:08 +0300

    -

--- cogs/gamesCog.py
+++ cogs/gamesCog.py
@@ -17,41 +17,7 @@ class GamesCog(Cog):
     def __del__(self):
         ...
 
-    @nextcord.slash_command(name="start", description="Group start command!", guild_ids=test_guilds)
-    async def create_game(self, interaction: Interaction):
-        """
-        Корень группы команд для создания игр. Нигде не используется.
-        """
-        pass
-
-    @create_game.subcommand(name="play", description="Create a new game!")
-    async def cirulli_game(
-        self,
-        interaction: Interaction,
-        game_name: str = SlashOption(
-            name="game",
-            description="The game you want",
-            choices=[
-                "2048",
-                "Tic Tac Toe",
-                "Checkers"
-            ]
-        ),
-    ):
-
-        """
-        Тут будет отправка сообщений о настройках игры и подключение новых кнопок (от классов игры)
-        """
-        if game_name == "2048":
-            await interaction.response.send_message(
-                f"2048"
-            )
-        elif game_name == "Tic Tac Toe":
-            await interaction.response.send_message(
-                f"Tic Tac Toe: X goes first", view=TicTacToe()
-            )
-        elif game_name == "Checkers":
-            await interaction.response.send_message(f"This is Checkers!")
+    
 
 
 def setup(bot: Bot) -> None:
