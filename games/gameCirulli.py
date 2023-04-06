9e36d465d489c24b98741ab28c6ad0ab68bf958e
iyunakov <iyunakov@notemail.ru>
2023-04-06 14:22:22 +0300

    -

--- games/gameCirulli.py
+++ games/gameCirulli.py
@@ -15,53 +15,6 @@ class GameCirulliStartView(nextcord.ui.View):
 
 
 class GameCirulliView(nextcord.ui.View):
-    def __init__(self, game):
-        raise "Данный класс ни где не используется!"
-        super().__init__()
-        self.game = game
-
-
-    @nextcord.ui.button(style=nextcord.ButtonStyle.secondary, label="\u200b", row=1, disabled=True)
-    async def puff_left(self, button: nextcord.ui.Button):
-        button.disabled = True
-        pass
-
-    @nextcord.ui.button(label="⬆", style=nextcord.ButtonStyle.green, row=1)
-    async def move_up_button(self, button: nextcord.ui.Button, interaction: nextcord.Interaction):
-        ...
-        self.game.move_up()
-        data = '\n'.join('\t'.join(map(str, row)) for row in self.game.data)
-        await interaction.response.edit_message(content=f"2048\n{data}", view=self)
-
-    @nextcord.ui.button(style=nextcord.ButtonStyle.secondary, label="\u200b", row=1, disabled=True)
-    async def puff_right(self, button: nextcord.ui.Button):
-        pass
-
-    @nextcord.ui.button(label="⬅", style=nextcord.ButtonStyle.green, row=2)
-    async def move_left_button(self, button: nextcord.ui.Button, interaction: nextcord.Interaction):
-        ...
-        self.game.move_left()
-        data = '\n'.join('\t'.join(map(str, row)) for row in self.game.data)
-        await interaction.response.edit_message(content=f"2048\n{data}", view=self)
-
-    @nextcord.ui.button(label="⬇", style=nextcord.ButtonStyle.green, row = 2)
-    async def move_down_button(self, button: nextcord.ui.Button, interaction: nextcord.Interaction):
-        ...
-        self.game.move_down()
-        data = '\n'.join('\t'.join(map(str, row)) for row in self.game.data)
-        await interaction.response.edit_message(content=f"2048\n{data}", view=self)
-    
-    @nextcord.ui.button(label="➡", style=nextcord.ButtonStyle.green, row=2)
-    async def move_right_button(self, button: nextcord.ui.Button, interaction: nextcord.Interaction):
-        ...
-        self.game.move_right()
-        data = '\n'.join('\t'.join(map(str, row)) for row in self.game.data)
-        await interaction.response.edit_message(content=f"2048\n{data}", view=self)
-
-    ...
-
-
-class GameCirulli(nextcord.ui.View):
     def __init__(self, size: int):
         """
         Конструктор
