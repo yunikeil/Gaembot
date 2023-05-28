7e3617d5792962a8b7344d18ff950f9e6ad5a492
yunik <yunik@notreal.notreal>
2023-05-28 22:55:58 +0000

    -

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 
 cd /root/vscode/python/Gaembot/
 source /root/vscode/python/Gaembot/Venv/bin/activate
-/root/vscode/python/Gaembot/Venv/bin/python3.10 /root/vscode/python/Gaembot/src/main.py &> /root/vscode/python/Gaembot/gaembot.txt & > /root/vscode/python/Gaembot/gaembot_proc_num.txt
+/root/vscode/python/Gaembot/Venv/bin/python3.10 /root/vscode/python/Gaembot/src/main.py &> /root/vscode/python/Gaembot/gaembot.txt &
 disown %
 jobs -l 
 
