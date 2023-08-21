307c6edf402bc7fd42201dcb88dddf5ca30774e7
prod server <iyunakov@inbox.ru>
2023-08-22 02:17:58 +0300

    moved to docs dir

--- start.sh
+++ start.sh
@@ -1,9 +1,10 @@
+#!/bin/bash
 
-cd /root/vscode/python/Gaembot/
-source /root/vscode/python/Gaembot/Venv/bin/activate
-/root/vscode/python/Gaembot/Venv/bin/python3.10 /root/vscode/python/Gaembot/src/main.py &> /root/vscode/python/Gaembot/gaembot.txt &
-disown %
-jobs -l 
+find . -maxdepth 1 -type f -name '[0-9]*.last' -exec rm {} \;
+rename 's/([0-9]+)/$1.last/' *
 
-
+source Venv/bin/activate
+python3 main.py &> temp_outut.txt &
+pid=$(pgrep -f "python3 main.py")
+touch "${pid}"
 
